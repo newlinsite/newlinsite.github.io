aa674cca0895e5bdaa0c4af6a426b020270f1b9f
newlin <qu4j8j8jj@gamil.com>
2019-03-30 21:25:37 +0800

    001

---
+++ soeye/js/top.js
@@ -0,0 +1,13 @@
+$(function(){
+	
+	$("#goTop").click(function(){
+
+		$("html,body").animate({scrollTop:0},900);
+
+		//$("html,body").animate({scrollTop:0},900,"easeOutBounce");
+
+		return false;
+
+	});
+
+});
