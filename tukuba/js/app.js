62f4f1d49a74cf73d70603fdab03b98bba21289d
newlin <31960199+newlinsite@users.noreply.github.com>
2023-01-28 18:10:34 +0800

    001

--- tukuba/js/app.js
+++ tukuba/js/app.js
@@ -12,6 +12,66 @@ var $tag = function (name) {
 
 
 
+//--------------------------------
+//
+//			input name , work image
+//
+//--------------------------------
+
+
+
+var workname = [
+	"portfolio",
+	//----------------------
+	"All-in-One Streaming Studio Solution Application.",
+	"Text-to-Speech AI Application",
+	"Future Training Room",
+	"uvc",
+	"Design system"
+]
+
+var workname_xs = [
+	"portfolio",
+	//----------------------
+	"d_uc",
+	"d_tts",
+	"d_ftr",
+	"d_uvc",
+	"d_ds"
+
+]
+
+
+
+
+
+//--------------------
+//	input index work name/link
+//--------------------
+if (workNumber == 0) {
+	var h3 = $tag("h3")
+	var link = $tag("a")
+	for (let i = 0; i < h3.length; i++) {
+		h3[i].innerHTML = workname[i + 1];
+		link[i].href = workname_xs[i + 1] + ".html"
+	}
+}
+
+
+//--------------------
+//	input work name and image
+//--------------------
+if (workNumber > 0) {
+	$tag("title")[0].innerHTML = workname[workNumber]
+	$tag("h1")[0].innerHTML = workname[workNumber]
+	var img = $tag("img")
+	for (let i = 0; i < img.length; i++) {
+		img[i].src = "image/" + workname_xs[workNumber] + i + ".png"
+	}
+}
+
+
+
 
 
 
@@ -30,8 +90,8 @@ var headerHtml = '\
 				<img src="image/logo.png" alt="" class="logo">\
 				<ul class="row flex-jus-end">\
 					<li class=""><a href="#">Works</a></li>\
-					<li class=""><a href="#">Blog</a></li>\
-					<li class=""><a href="#">About</a></li>\
+					<li class=""><a href="about.html">Blog</a></li>\
+					<li class=""><a href="about.html">About</a></li>\
 				</ul>\
 				<div class="sidebar row flex-dir-col">\
 					<a class="" href="index.html">home</a>\
@@ -52,15 +112,6 @@ footer[0].innerHTML = footerHtml;
 
 
 
-
-
-
-
-
-
-
-
-
 // Header smaller
 
 document.addEventListener('scroll', function (e) {
@@ -70,6 +121,7 @@ document.addEventListener('scroll', function (e) {
 		header[0].classList.remove("sm");
 	}
 })
+
 
 
 
