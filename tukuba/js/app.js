d4879f1fb5dae9eee2e4dce01030cba9a18e8d0c
newlin <31960199+newlinsite@users.noreply.github.com>
2023-02-19 23:50:41 +0800

    001

--- tukuba/js/app.js
+++ tukuba/js/app.js
@@ -62,9 +62,9 @@ var worknameBoxFull = [
 
 
 //查找作品順序
-console.log(workName)
-var workNumber = worknameBox.indexOf(workName)
 
+var workNumber = worknameBox.indexOf(workName)
+console.log(workName)
 console.log(workNumber)
 
 
@@ -82,7 +82,7 @@ if (workNumber == 0) {
 
 
 //--------------------
-//	input work name and image
+//	input work name , image , next/last work
 //--------------------
 if (workNumber > 0) {
 	$tag("title")[0].innerHTML = worknameBoxFull[workNumber].replace('</br>', '')
@@ -98,7 +98,38 @@ if (workNumber > 0) {
 		img[i].src = "image/" + worknameBox[workNumber] + i + imgType
 		console.log(img[i].src)
 	}
+
+	//導入 side bar
+	try {
+		$("sidebar").classList.add("row")
+		$("sidebar").classList.add("flex-dir-col")
+		$("sidebar").innerHTML = '<div class="row flex-dir-col">\
+				<a class="mt-xl" href="index.html">Home</a>\
+				<a id="nextwork" class="icon" href="#">chevron_right<div id="nextwork-describe"></div></a>\
+				<a id="lastwork" class="icon" href="#">chevron_left<div id="lastwork-describe"></div></a>\
+			</div>\
+			<a href="#">Top</a>'
+
+		//導入上下一個作品按鈕連結
+		if (workNumber == 1) {
+			$("lastwork").classList.add("hidden")
+		} else if (workNumber == worknameBox.length) {
+			$("nextwork").classList.add("hidden")
+		}
+		$("nextwork").href = worknameBox[workNumber + 1] + ".html#img01"
+		$("lastwork").href = worknameBox[workNumber - 1] + ".html#img01"
+		$("nextwork-describe").innerHTML = worknameBoxFull[workNumber + 1]
+		$("lastwork-describe").innerHTML = worknameBoxFull[workNumber - 1]
+	} catch {
+		console.log("There is not side bar")
+	}
+
 }
+
+
+
+
+
 
 
 
@@ -115,17 +146,16 @@ var footer = $tag("footer")
 
 // input Header
 var headerHtml = '\
-			<div class="container row flex-align-end">\
-				<a href="index.html"><img src="image/logo.png" alt="" class="logo"></a>\
+			<div class="container">\
+				<a href="index.html">\
+					<div class="logo">\
+						<div></div>\
+					</div>\
+				</a>\
 				<ul class="row flex-jus-end">\
 					<li><a href="index.html">Works</a></li>\
-					<!-- <li><a href="about.html">Blog</a></li> -->\
 					<li><a href="about.html">Resume</a></li>\
 				</ul>\
-				<!--<div class="sidebar row flex-dir-col">\
-					<a href="index.html">home</a>\
-					<a class="block now" href="index.html">Works</a>\
-				</div>-->\
 			</div>'
 
 header[0].innerHTML = headerHtml;
@@ -142,13 +172,21 @@ footer[0].innerHTML = footerHtml;
 
 
 // Header smaller
-
+var LastScrollY = 0
 document.addEventListener('scroll', function (e) {
-	if (window.scrollY != 0) {
+	let scrollY = window.scrollY
+	if (scrollY == 0) {
+		header[0].classList.remove("sm");
+		header[0].classList.remove("md");
+	} else if (scrollY > LastScrollY) {
+		header[0].classList.remove("md");
 		header[0].classList.add("sm");
 	} else {
 		header[0].classList.remove("sm");
+		header[0].classList.add("md");
 	}
+
+	LastScrollY = scrollY
 })
 
 
