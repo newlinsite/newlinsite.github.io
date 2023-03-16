85f0e1101be8736cef073f84620d40def1d33b29
newlin <31960199+newlinsite@users.noreply.github.com>
2023-03-16 09:51:39 +0800

    001

--- tukuba/js/image_nexter.js
+++ tukuba/js/image_nexter.js
@@ -24,31 +24,9 @@ function createNextBtn() {
             }
             break;
         }
-        // lastBtn.href = "#img0" + scrollSession.length
     }
 }
 
-
-
-// function createNextBtn() {
-//     for (i = 0; i < scrollSession.length; i++) {
-//         if (window.scrollY > scrollSession[i].offsetTop) {
-//             let j = i + 2
-//             lastBtn.href = "#img0" + i
-//             nextBtn.href = "#img0" + j
-//             lastBtn.classList.remove("hidden")
-//             nextBtn.classList.remove("hidden")
-//             if (i == 0) {
-//                 lastBtn.classList.add("hidden")
-//             }
-//             if (i == scrollSession.length - 2) {
-//                 nextBtn.classList.add("hidden")
-//             }
-//         } else {
-//             break;
-//         }
-//     }
-// }
 
 
 window.onload = function () {
