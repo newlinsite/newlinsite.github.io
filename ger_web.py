f68369d6dedd1f8582fa6ec7ffd0fbe1e34ea888
newlin <31960199+newlinsite@users.noreply.github.com>
2023-07-21 18:58:10 +0800

    001

--- ger_web.py
+++ ger_web.py
@@ -25,7 +25,7 @@ url_template = ["https://www.managertoday.com.tw/{}","?page=","/view/"]
 #url_type = "articles"
 #url_type = "books"
 url_type = "articles/category/project"
-url_indexPage = 12
+url_indexPage = 2
 
 
 user_agent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36' #偽裝使用者
@@ -47,12 +47,22 @@ for pageNo in range(url_indexPage):
     index_res2 = re.findall(r'<span>(\d{4}-\d{2}-\d{2})</span>.*?<a href="/(.*?)/view/(\d+)" title="(.*?)">',index_res2)
           
     # 將提取的資料轉換為 DataFrame
-    index_data = pd.DataFrame(index_res2, columns=['date','articleType','articleNo', 'title'])
-    index_df = index_df.append(index_data, ignore_index=False, verify_integrity=False)
+    index_data = pd.DataFrame(index_res2, columns=['date','articleType','articleNo', 'title'])  
+    index_df =  pd.concat([index_df, index_data], ignore_index=True)
+        
     
     time.sleep(random.uniform(1,3))
 
 index_df = index_df.drop_duplicates(subset=['articleNo']).reset_index(drop=True)
+
+
+
+
+
+
+
+
+
 
 
 
@@ -85,13 +95,15 @@ for i in range(len(index_df)):
             article_body = res_text[start_index:end_index]
             article_body = str(article_body.encode('utf-8').decode('unicode-escape'))
 
-    data = pd.DataFrame({'content': [article_body]})
-    df = df.append(data, ignore_index=False, verify_integrity=False)
+    data = pd.DataFrame({'content': [article_body]})     
+    df =  pd.concat([df, data], ignore_index=True)
+    
+    
     
     #取出英文字
     english_words = re.findall(r'[a-zA-Z]+', article_body)
     data_en = pd.DataFrame({'english_words': english_words})
-    df_en = df_en.append(data_en, ignore_index=False, verify_integrity=False)
+    df_en  =  pd.concat([df_en , data_en], ignore_index=True)
 
     
     time.sleep(random.uniform(1,3))
