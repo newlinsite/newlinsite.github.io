ce043dbcd544432b561e1dc28f9c2495286157ce
newlin <31960199+newlinsite@users.noreply.github.com>
2024-01-12 00:50:41 +0800

    001

--- program/aten/MRS/space_control.js
+++ program/aten/MRS/space_control.js
@@ -754,24 +754,50 @@ ipadElementAtr[0] = [
         bgScr: "image/test_bg.png",
         btns: [
             {
-                xy: [30, 50], size: [20, 20], radius: 4, border: 3,
+                xy: [25, 45], size: [24.5, 17], radius: 4, border: 3,
                 bgColor: ["#000", "#333"], bgScr: ["", ""], borderColor: ["#333", "#111"],
                 text: "PIP Mode", textColor: "#fff", textSize: 20,
                 act: [() => { vp.changeLayout(display[1], "pip") }, () => { vp.changeLayout(display[1], "pop") }], toggle: true
             }, {
-                xy: [70, 50], size: [20, 20], radius: 4, border: 3,
-                bgColor: ["#000", "#333555"], bgScr: ["", ""], borderColor: ["#333", "#111"],
-                text: "Mixer", textColor: "#fff", textSize: 20,
+                xy: [50, 45], size: [24.5, 17], radius: 4, border: 3,
+                bgColor: ["#000", "#333"], bgScr: ["", ""], borderColor: ["#333", "#111"],
+                text: "source", textColor: "#fff", textSize: 20,
+                act: [() => { display[2].input(media[1]) }, () => { display[2].input(media[0]) }], toggle: true
+            }, {
+                xy: [75, 45], size: [24.5, 17], radius: 4, border: 3,
+                bgColor: ["#000", "#333"], bgScr: ["", ""], borderColor: ["#333", "#111"],
+                text: "PIP", textColor: "#fff", textSize: 20,
+                act: [() => { vp.changeLayout(display[3], "4x") }, () => { vp.changeLayout(display[3], [3, 3]) }], toggle: true
+            }, {
+                xy: [84, 15], size: [16, 8], radius: 4, border: 1,
+                bgColor: ["#00000055", "#333555"], bgScr: ["", ""], borderColor: ["#333", "#111"],
+                text: "Mixer", textColor: "#fff", textSize: 15,
                 act: () => { ipad.goTo(1) }, toggle: false
             }, {
                 xy: [85, 90], size: [16, 8], radius: 6, border: 0,
                 bgColor: ["#00000055", ""], bgScr: ["", ""], borderColor: ["#888", "#111"],
                 text: "50", textColor: "#aaa", textSize: 15,
                 act: "none", toggle: false
+            }, {
+                xy: [50, 70], size: [40, 8], radius: 10, border: 0,
+                bgColor: ["#00000055", "#00BBFF77"], bgScr: ["", ""], borderColor: ["#333", "#111"],
+                text: "< >", textColor: "#fff", textSize: 15,
+                act: [() => {
+                    display[1].move([6, 0, 0], [0, 0, 0])
+                    display[3].move([-6, 0, 0], [0, 0, 0])
+                }, () => {
+                    display[1].move([-6, 0, 0], [0, 0, 0])
+                    display[3].move([6, 0, 0], [0, 0, 0])
+                }], toggle: true
+            }, {
+                xy: [94, 45], size: [10, 14], radius: 4, border: 3,
+                bgColor: ["#000", "#333"], bgScr: ["", ""], borderColor: ["#333", "#111"],
+                text: "Proj.", textColor: "#fff", textSize: 14,
+                act: [() => { display[4].input(media[1]) }, () => { display[4].input(media[0]) }], toggle: true
             }
         ],
         sliders: [
-            { xy: [50, 90], size: 50, dir: "", value: 50, bgColor: "#000000", activeColor: "#fff", act: (e) => { ipad.page[0].btn[2].changeText(e) } }
+            { xy: [50, 90], size: 50, dir: "", value: 50, bgColor: "#000000", activeColor: "#fff", act: (e) => { ipad.page[0].btn[4].changeText(e) } }
         ]
     }, {
         pageID: 102,
@@ -1168,10 +1194,10 @@ var vp = new VP([
     media[0],
     media[1],
     media[1],
-    // media[0],
-    // media[1],
+    media[0],
+    media[1],
     //-------------------
-    // media[0],
+    media[0],
     media[0]])
 
 
@@ -1228,10 +1254,13 @@ display[4].input(media[1])
 
 display[0].hidden()
 
-ipad.on()
+// ipad.on()
 
 
-
+var ipadBtn = $css("ipadBtn")[0]
+ipadBtn.addEventListener("click", () => {
+    ipad.onToggle()
+})
 
 ///---------------------------------
 //
